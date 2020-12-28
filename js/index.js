34d22b2244c051c47dbdb2695083d88b4578bdcb
vrsvr <rifatvarisov@gmail.com>
2020-12-28 17:47:35 +0500

    Закрытия модального окна

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
-const modal = $.modal();
+const modal = $.modal({
+  title: 'Modal window title',
+  closable: true,
+  content: `
+    <p>Lorem ipsum dolor</p>
+  `,
+  width: '400px',
+  footerButtons: [{
+    text: 'Ok',
+    type: 'primary',
+    handler() {
+      console.log('Primary btn clicked');
+      modal.close();
+    }
+  }, {
+    text: 'Cancel',
+    type: 'danger',
+    handler() {
+      console.log('Danger btn clicked');
+      modal.close();
+    }
+  }, ]
+});
