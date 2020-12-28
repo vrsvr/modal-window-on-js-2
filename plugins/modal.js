4e67210f85507bf61b1acf0d3ab2e82e9d1e847d
vrsvr <rifatvarisov@gmail.com>
2020-12-28 13:17:29 +0500

    Логика открытия / закрытия модального окна

--- plugins/modal.js
+++ plugins/modal.js
@@ -1,3 +1,38 @@
-$.modal = function() {
-  
+function _createModal(options) {
+  const modal = document.createElement('div');
+  modal.classList.add('vmodal');
+  modal.insertAdjacentHTML('afterbegin', `
+    <div class="modal-overlay">
+      <div class="modal-window">
+        <div class="modal-header">
+          <span class="modal-title">Modal title</span>
+          <span class="modal-close">&times;</span>
+        </div>
+        <div class="modal-body">
+          <p>Lorem ipsum dolor sit.</p>
+          <p>Lorem ipsum dolor sit.</p>
+        </div>
+        <div class="modal-footer">
+          <button>Ok</button>
+          <button>Cancel</button>
+        </div>
+      </div>
+    </div>
+  `);
+  document.body.appendChild(modal);
+  return modal;
+}
+
+$.modal = function (options) {
+  const $modal = _createModal(options);
+
+  return {
+    open() {
+      $modal.classList.add('open');
+    },
+    close() {
+      $modal.classList.remove('open');
+    },
+    destroy() {}
+  };
 };
